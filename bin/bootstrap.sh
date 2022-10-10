45ce9c68b505812a4f72a2937e6bc1413db0cada
Max Paulus 🥪 <maxpaulu@amazon.com>
2022-10-10 13:49:32 -0700

    edit bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 
 cd ~ & mkdir c && cd c
-git clone git@github.com:maxpaulus43/dotfiles.git
-
-brew install bash
-brew install fzf
-brew install git
-brew install lazygit
-brew install mackup
-brew install neovim
-brew install ripgrep
-brew install tmux
-brew install tree
+git clone git@github.com:maxpaulus43/dotfiles.git && cd ~
 
 platform='unknown'
 unamestr=$(uname)
+
 if [[ "$unamestr" == 'Linux' ]]; then
   platform='linux'
 elif [[ "$unamestr" == 'FreeBSD' ]]; then
@@ -23,17 +14,25 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
   platform='macos'
 fi
 
-cp ~/c/dotfiles/mackup/.mackup.cfg ~/.mackup.cfg
-mackup restore
 
 mkdir ~/.nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 nvm install 'lts/*'
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-~/.tmux/plugins/tpm/bin/install_plugins
+#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#~/.tmux/plugins/tpm/bin/install_plugins
 
 if [[ $platform == 'macos' ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  brew install fzf
+  brew install git
+  brew install lazygit
+  brew install neovim
+  brew install ripgrep
+  brew install tmux
+  brew install tree
+
   brew install --cask alacritty
   brew install --cask spotify
   brew install --cask visual-studio-code
@@ -41,8 +40,13 @@ if [[ $platform == 'macos' ]]; then
   brew install --cask google-drive
   brew install --cask typora
   brew install --cask telegram
+
   brew tap homebrew/cask-fonts 
   brew install --cask font-hack-nerd-font
+
+  brew install mackup
+  cp ~/c/dotfiles/Mackup/.mackup.cfg ~/.mackup.cfg
+  mackup restore
 fi
 
 
