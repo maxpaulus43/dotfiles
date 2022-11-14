167d7945a7dedf06c2f734d325743bec2c3e4dab
Max Paulus 🥪 <maxpaulu@amazon.com>
2022-11-14 12:15:41 -0800

    null-ls, deno, remove redundant keymaps

--- Mackup/.config/nvim/lua/user/lsp/null-ls.lua
+++ Mackup/.config/nvim/lua/user/lsp/null-ls.lua
@@ -8,17 +8,19 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
--- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettierd.with({
 			extra_filetypes = { "toml" },
 		}),
-		formatting.eslint_d,
+		-- formatting.rome,
+		-- formatting.eslint_d,
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		formatting.google_java_format,
+
+		diagnostics.eslint_d,
 		diagnostics.flake8,
 	},
 })
