5bb4afabe24ce91980300b5d83e09e7ae848e278
Max Paulus 🥪 <maxpaulu@amazon.com>
2023-10-20 21:08:31 -0700

    add borders to stuff

--- Mackup/.config/nvim/lua/my/lsp.lua
+++ Mackup/.config/nvim/lua/my/lsp.lua
@@ -24,6 +24,23 @@ local servers = {
   },
 }
 
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = 'rounded',
+    close_events = { "BufHidden", "InsertLeave" },
+  }
+)
+
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = 'rounded',
+  }
+)
+
+vim.diagnostic.config({
+  float = { border = 'rounded' },
+})
+
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
