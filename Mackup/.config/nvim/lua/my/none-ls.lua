9ee54bfa8768bd500f55c995ba78130a478c6f1c
Max Paulus 🥪 <maxpaulu@amazon.com>
2024-01-08 15:20:23 -0800

    update urls

--- Mackup/.config/nvim/lua/my/none-ls.lua
+++ Mackup/.config/nvim/lua/my/none-ls.lua
@@ -3,12 +3,14 @@ if not null_ls_status_ok then
   return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
+-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
 local code_actions = null_ls.builtins.code_actions
+
+-- local hover = null_ls.builtins.hover
 
 null_ls.setup({
   debug = false,
