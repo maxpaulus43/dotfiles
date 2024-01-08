898904d78f3e9d5c9d8bc00a95ff392e965b86c2
Max Paulus 🥪 <maxpaulu@amazon.com>
2024-01-08 15:12:24 -0800

    use none-ls again. it's better

--- Mackup/.config/nvim/lua/my/conform.lua
+++ Mackup/.config/nvim/lua/my/conform.lua
@@ -1,14 +1,11 @@
+local prettier = { "prettier", "prettierd" }
 return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      javascript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescript = { "prettier" },
-      typescriptreact = { "prettier" },
-      html = { "prettier" },
-      css = { "prettier" },
+      html = prettier,
+      css = prettier,
       python = { "black" },
     },
     format_on_save = {
--- Mackup/.config/nvim/lua/my/none-ls.lua
+++ Mackup/.config/nvim/lua/my/none-ls.lua
@@ -7,6 +7,8 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actionslocal diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
   debug = false,
@@ -15,7 +17,17 @@ null_ls.setup({
       extra_filetypes = { "toml" },
     }),
     diagnostics.eslint_d,
+    code_actions.eslint_d,
+
+    formatting.stylua,
+
     diagnostics.flake8,
     formatting.black.with({ extra_args = { "--fast" } }),
+
+    code_actions.shellcheck,
+    diagnostics.shellcheck,
+    formatting.shfmt,
+
+    code_actions.gitsigns,
   },
 })
