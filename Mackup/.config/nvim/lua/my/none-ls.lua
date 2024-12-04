101b9fa81750a78ed8928bf4b4284a05a38080b0
Max Paulus 🥪 <maxpaulu@amazon.com>
2024-12-03 20:18:42 -0800

    zed

--- Mackup/.config/nvim/lua/my/none-ls.lua
+++ Mackup/.config/nvim/lua/my/none-ls.lua
@@ -47,18 +47,18 @@ return {
 				require("none-ls-shellcheck.code_actions"),
 				formatting.shfmt,
 			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
+			-- on_attach = function(client, bufnr)
+			-- 	if client.supports_method("textDocument/formatting") then
+			-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			-- 		vim.api.nvim_create_autocmd("BufWritePre", {
+			-- 			group = augroup,
+			-- 			buffer = bufnr,
+			-- 			callback = function()
+			-- 				vim.lsp.buf.format({ async = true })
+			-- 			end,
+			-- 		})
+			-- 	end
+			-- end,
 		})
 	end,
 }
